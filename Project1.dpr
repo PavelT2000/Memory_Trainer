627d186b656a877fea1d59049f40dcb999a8ac4b
PavelT2000 <pavelzabelich228@gmail.com>
2024-12-12 15:32:45 +0300

    commit

--- Project1.dpr
+++ Project1.dpr
@@ -8,10 +8,5 @@ uses
   System.SysUtils;
 
 begin
-  try
-    { TODO -oUser -cConsole Main : Insert code here }
-  except
-    on E: Exception do
-      Writeln(E.ClassName, ': ', E.Message);
-  end;
+  Writeln('Hello world');
 end.
